769f20286e45c5f4501c47a03693ce0dd71f4ba6
Alex Monteiro <alex.monteiro@digiage.com.br>
2021-03-16 09:19:20 -0300

    fixing errors

--- src/index.js
+++ src/index.js
@@ -10,19 +10,62 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(person => person.username === username);
+
+  if (!user) return response.status(404).json({ error: "User not found!" })
+
+  request.user = user;
+  request.username = username;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const total = user.todos;
+
+  if (user.pro === false && total.length >= 10) return response.status(403).json({ error: "User alread have 10 todos!" })
+
+  request.user = user;
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  var valide = validate(id);
+
+  const user = users.find(person => person.username === username);
+
+  if (!valide) return response.status(400).json({ error: "Bad request! Id not on the right format" })
+
+  const todoExists = users.find(person => {
+    return person.todos.find(todo => todo.id === id);
+  })
+
+  if (!todoExists) return response.status(404).json({ error: "Todo not found!" });
+
+  request.todo = todoExists.todos[0];
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(person => person.id === id);
+
+  if (!user) return response.status(404).json({ error: "User not found!" })
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
